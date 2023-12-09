d6834e0446eaaa1ccc6a4b64bb3d6e66cb771892
abd <abdalhababseh@outlook.com>
2023-12-09 21:21:53 +0300

    hi

--- JobPortal/src/userDashBoard/SkillSwapRequests/Friends.js
+++ JobPortal/src/userDashBoard/SkillSwapRequests/Friends.js
@@ -1,55 +1,56 @@
-$(".card").on("click", function(){
-    $(".detail").addClass("active");
+$(".card").on("click", function () {
+  $(".detail").addClass("active");
 });
 
-$(".close-detail").on("click", function(){
-    $(".detail").removeClass("active");
+$(".close-detail").on("click", function () {
+  $(".detail").removeClass("active");
 });
 
-$(".menu-bar").on("click", function(){
-    $(".sidebar").addClass("active");
+$(".menu-bar").on("click", function () {
+  $(".sidebar").addClass("active");
 });
 
-$(".logo").on("click", function(){
-    $(".sidebar").removeClass("active");
+$(".logo").on("click", function () {
+  $(".sidebar").removeClass("active");
 });
 
 // Function to fetch pending swap requests
 function fetchPendingRequests(userId) {
-    // Replace 'SESSION_USER_ID' with the actual way to retrieve the user ID from the session
-    // let userId = 'SESSION_USER_ID'; // Get the user ID from the session
+  // Replace 'SESSION_USER_ID' with the actual way to retrieve the user ID from the session
+  // let userId = 'SESSION_USER_ID'; // Get the user ID from the session
 
-    fetch('http://localhost/MasterPieseAPIsGithub/MasterPieseAPIs/server/User/skillSwapRequests/selectUserAcceptedRequests.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ UserID: userId })
+  fetch(
+    "http://localhost/MasterPieseAPIsGithub/MasterPieseAPIs/server/User/skillSwapRequests/selectUserAcceptedRequests.php",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ UserID: userId }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message) {
+        console.log(data.message);
+      } else {
+        displayPendingRequests(data);
+      }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.message) {
-         console.log(data.message)
-        } else {
-            displayPendingRequests(data);
-        }
-    })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 }
 
-
-
 function displayPendingRequests(requests) {
-    let container = document.getElementById('pendingRequestsContainer');
-    
-    container.innerHTML = ''; // Clear previous content
-    
-    requests.forEach(request => {
-        let card = document.createElement('div');
-        card.classList.add('col-md-6', 'col-xl-4'); // Add Bootstrap classes
-        
-        // Structure the card with fetched data
-        card.innerHTML = `
+  let container = document.getElementById("pendingRequestsContainer");
+
+  container.innerHTML = ""; // Clear previous content
+
+  requests.forEach((request) => {
+    let card = document.createElement("div");
+    card.classList.add("col-md-6", "col-xl-4"); // Add Bootstrap classes
+
+    // Structure the card with fetched data
+    card.innerHTML = `
         <div class="card">
         <div class="card-body d-flex flex-column">
         <div class="media align-items-center">
@@ -72,41 +73,35 @@ function displayPendingRequests(requests) {
         </div>
         </div>
         `;
-        
-        container.appendChild(card);
-    });
+
+    container.appendChild(card);
+  });
 }
-
-
 
 function deleteFriend(RequestID) {
-    console.log(RequestID)
-    fetch('http://localhost/MasterPieseAPIsGithub/MasterPieseAPIs/server/User/skillSwapRequests/deleteFriend.php', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ RequestID: RequestID })
+  console.log(RequestID);
+  fetch(
+    "http://localhost/MasterPieseAPIsGithub/MasterPieseAPIs/server/User/skillSwapRequests/deleteFriend.php",
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ RequestID: RequestID }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message); // Log the response message
+      // Optionally update the UI or perform additional actions based on the response
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message); // Log the response message
-        // Optionally update the UI or perform additional actions based on the response
-    })
-    .catch(error => console.error('Error:', error));
-
+    .catch((error) => console.error("Error:", error));
 }
 
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    
-    // let userID = sessionStorage.getItem("userid");
-    let userID=2
-    if(!userID)(
-        userID=2
-        )
-        fetchPendingRequests(2);
-    });
-    
+document.addEventListener("DOMContentLoaded", function () {
+  let userID = sessionStorage.getItem("userid");
+  
+  if (!userID)
+  {userID = 2;} 
+  fetchPendingRequests(2);
+});
